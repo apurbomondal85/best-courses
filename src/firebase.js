3bbfab72e204de05fb9a45b5bf0f054040e84cc9
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2023-11-29 13:39:28 +0600

    some fixed firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_APIKEY,
-    authDomain: import.meta.env.VITE_AUTHDOMAIN,
-    projectId: import.meta.env.VITE_PROJECTID,
-    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-    appId: import.meta.env.VITE_APPID
+    apiKey: "AIzaSyD9D5CW945tr9a1_P4hcCml_Cza1iOS_HU",
+    authDomain: "best-course-e34e9.firebaseapp.com",
+    projectId: "best-course-e34e9",
+    storageBucket: "best-course-e34e9.appspot.com",
+    messagingSenderId: "621311724732",
+    appId: "1:621311724732:web:e0fd10c868f011c2c0d4b9",
 };
 
 // Initialize Firebase
